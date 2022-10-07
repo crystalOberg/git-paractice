8f6a87f16ad8ac3f83e4cac1c12a86703ac9a151
Crystal <crystal.ochoa03@gmail.com>
2022-10-07 21:56:52 +0200

    completed the funcions  exercise

--- functions.js
+++ functions.js
@@ -1,10 +1,42 @@
 "use strict";
 
-function Hello(name) {
-    return "Hello " + name + "!";
+
+function sayHello(name) {
+    return "Hello, " + name;
 }
-{   console.log('Hello whats your name?');
+
+   let helloMessage = sayHello(name);
+    console.log(helloMessage);
+
+    let myName = "Crystal Moon";
+    console.log(sayHello(myName));
+
+function isTwo(number) {
+    return number === 2;
+
+    console.log(isTwo(random));
+}
+    function calculateTip(tipPercent, total) {
+    let tipAmount = total * tipPercent;
+    return tipAmount;
+}
+    {
+        console.log(calculatedTip(tipPercent:.2, total:.20));
+        console.log(calcucatedTip(tipPercent:.0.25, total:,25.50))
+        console.log(calculatedTip(tipPercent:.0.15, total:.33.42));
+    }
+    let userTotal = prompt(message: "Hey what was your total at Chili's today?");
+    let userTip = parseFloat(prompt(message: "What percentage would you like to tip?")/100;
 
 
-    var Crystal = ('Hello Crystal!');
-}
+    alert("you'll add $" + calculateTip(userTip, userTotal) + " to your bill today.");{
+
+        function applyDiscount(itemPrice, itemDiscount) {
+        let disPrice = itemPrice - (itemPrice + itemDiscount);
+        return disPrice;
+
+    }
+
+    console.log(applyDiscount(itemPrice: 100, itemDiscount:.2));
+    console.log(applyDiscount(itemPrice: 45.99, itemDiscount:0.12));
+
